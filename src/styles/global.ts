bbc200b2467f4a90bf26f74dcf498f83fe3ea2dd
gdegato <rosele.tst@gmail.com>
2023-10-20 21:04:54 -0300

    Customizando barra de rolagem

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,13 +4,25 @@ export const GlobalStyle = createGlobalStyle`
 
 *{   
     padding: 0;
-    box-sizing: border-box;      
+    box-sizing: border-box;    
 }
 
  :focus {
         outline: 0 ;
         box-shadow: 0 0 0 2px ${(props) => props.theme['purple']};
     }
+::-webkit-scrollbar{
+    width: 20px;    
+}
+
+::-webkit-scrollbar-track{
+    background: ${(props) => props.theme['base-profile']};
+}
+
+::-webkit-scrollbar-thumb{
+    background: ${(props) => props.theme['base-border']};
+    border-radius: 2px;
+}
 
     body {
         background:${(props) => props.theme['base-background']};
